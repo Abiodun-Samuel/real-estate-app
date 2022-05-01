47438800f90604c8b731cd86edd9f1d8101ebd0c
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-05-01 21:47:56 +0100

    hero section

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -4,14 +4,29 @@ import { Box, Flex, Text } from "@chakra-ui/react";
 const Hero = () => {
   return (
     <div className="hero">
-      {/* <Flex flexWrap="wrap" justifyContent="center" alignItems="center" m="30"> */}
-      <Text>Lorem, ipsum dolor.</Text>
-      <Text>
-        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum
-        assumenda laborum consectetur corporis temporibus? Delectus fugiat
-        magnam temporibus cum soluta?
-      </Text>
-      {/* </Flex> */}
+      <Flex
+        flexWrap="wrap"
+        justifyContent="center"
+        alignItems="center"
+        m="10"
+        textAlign="center"
+      >
+        <Box minW={300} maxW={600}>
+          <Text
+            fontSize="4xl"
+            color="#ffffff"
+            fontWeight="bold"
+            marginBottom="5"
+          >
+            Lorem, ipsum dolor.
+          </Text>
+          <Text fontSize="lg" color="#dddddd">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Voluptatum
+            assumenda laborum consectetur corporis temporibus? Delectus fugiat
+            magnam temporibus cum soluta?
+          </Text>
+        </Box>
+      </Flex>
     </div>
   );
 };
