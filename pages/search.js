034e94bc0b581a3116aa1e9c7cf264e7780efae2
Abiodun Samuel <abiodunsamyemi@gmail.com>
2022-05-01 04:30:54 +0100

    search property

--- pages/search.js
+++ pages/search.js
@@ -8,7 +8,7 @@ import Property from "../components/Property";
 // import SearchFilters from "../components/SearchFilters";
 import { baseUrl, fetchApi } from "../utils/fetchApi";
 import SearchFilters from "../components/SearchFilters";
-// import noresult from "../assets/images/noresult.svg";
+import noresult from "../assets/images/no-result.jpg";
 
 const Search = ({ properties }) => {
   const [searchFilters, setSearchFilters] = useState(false);
@@ -36,11 +36,11 @@ const Search = ({ properties }) => {
         Properties {router.query.purpose}
       </Text>
       <Flex flexWrap="wrap">
-        {/* {properties.map((property) => (
+        {properties.map((property) => (
           <Property property={property} key={property.id} />
-        ))} */}
+        ))}
       </Flex>
-      {/* {properties.length === 0 && (
+      {properties.length === 0 && (
         <Flex
           justifyContent="center"
           alignItems="center"
@@ -53,9 +53,32 @@ const Search = ({ properties }) => {
             No Result Found.
           </Text>
         </Flex>
-      )} */}
+      )}
     </Box>
   );
 };
+
+export async function getServerSideProps({ query }) {
+  const purpose = query.purpose || "for-rent";
+  const rentFrequency = query.rentFrequency || "yearly";
+  const minPrice = query.minPrice || "0";
+  const maxPrice = query.maxPrice || "1000000";
+  const roomsMin = query.roomsMin || "0";
+  const bathsMin = query.bathsMin || "0";
+  const sort = query.sort || "price-desc";
+  const areaMax = query.areaMax || "35000";
+  const locationExternalIDs = query.locationExternalIDs || "5002";
+  const categoryExternalID = query.categoryExternalID || "4";
+
+  const data = await fetchApi(
+    `${baseUrl}/properties/list?locationExternalIDs=${locationExternalIDs}&purpose=${purpose}&categoryExternalID=${categoryExternalID}&bathsMin=${bathsMin}&rentFrequency=${rentFrequency}&priceMin=${minPrice}&priceMax=${maxPrice}&roomsMin=${roomsMin}&sort=${sort}&areaMax=${areaMax}`
+  );
+
+  return {
+    props: {
+      properties: data?.hits,
+    },
+  };
+}
 
 export default Search;
